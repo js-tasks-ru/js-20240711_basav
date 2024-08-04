efea21a8832b69f45f20342822f05b7660f9055d
Anna Basova <anna.basova@stalcom.com>
2024-08-04 23:13:46 +0300

    3,4,5 task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const fields = path.split('.');
+     return  function(GetPath) {
+        let value = GetPath;
+        for (const prop of fields) 
+            if (Object.hasOwn(value, prop)){
+                value = value[prop];
+            } else {
+                return;
+            }
+        return value;
+    }
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,7 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+    var NewArr = arr;
+    let uniqueList= [...new Set(NewArr)];
+    return uniqueList;
 }
